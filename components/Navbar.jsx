b777644b3c7c27eedd41cea6759ce847f400d61b
estherniyo <estherniyonkuru123@gmail.com>
2023-02-15 16:33:15 +0200

    changed the logo format for compactibility with deployment

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -4,7 +4,11 @@ const Navbar = () => {
   return (
     <div className="px-12 shadow-2xl flex items-center max-w-7xl mx-auto py-6 mb-12 ">
       {" "}
-      <img src="../src/assets/Vector.svg" alt="" className="h-[40px]" />
+      <img
+        src="https://i.postimg.cc/R0yLz0WK/Vector.pnghttps://i.postimg.cc/R0yLz0WK/Vector.png"
+        alt=""
+        className="h-[40px]"
+      />
     </div>
   );
 };
